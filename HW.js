8e4becd93b4313431df0a1943769544ed1f121a3
Michael Anderson <ratboyjim@gmail.com>
2020-11-16 18:13:24 +0000

    Q2 answered

--- HW.js
+++ HW.js
@@ -1,15 +1,34 @@
-const scenario = {
-    murderer: 'Miss Scarlet',
-    room: 'Library',
-    weapon: 'Rope'
-  };
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+//   };
   
-  const declareMurderer = function() {
-    return `The murderer is ${scenario.murderer}.`;
-  }
+//   const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+//   }
   
-  const verdict = declareMurderer();
-  console.log(verdict);
+//   const verdict = declareMurderer();
+//   console.log(verdict);
 
-  // The log show show `The murderer is Miss Scarlet' 
-  // as the scenario variable is a constant and can be accessed by the declareMurderer function. 
+  // The log should show `The murderer is Miss Scarlet' 
+  // as the scenario variable is a constant and can be accessed by the declareMurderer function.
+
+
+  const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The log should show 'The murderer is Professor Plum' 
+// as the murderer variable is a constant and cannot be changed.
+// Or it will just return an error.
